d57e5261ee75c965a88f93d9204e0e8f5d578f4e
elliot <eting.jh@gmail.com>
2024-11-15 11:12:54 +0800

    changed createUser interface form title

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login', 'index.html'));
 });
 
+app.get('/createUser', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'createUser', 'createUser.html'));
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
